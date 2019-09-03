bf370645bc17fa3fb2e645e6c2063754719949f3
morganamilo <morganamilo@gmail.com>
2019-09-03 21:09:42 +0100

    Update for raur-2.0.0

--- src/raur.rs
+++ src/raur.rs
@@ -1,19 +1,18 @@
 use crate::Cache;
-use raur::{Error, Handle, Package};
+use raur::{Package, Raur};
 
 /// Extension functions to raur::Package
-pub trait RaurExt {
+pub trait RaurExt: Raur {
     /// Perform an info request, storing the results into cache. Requests are not made
     /// for packages already in cache. If all packages are already in cache then this
     /// is effectivley a noop.
-    fn cache_info<S: AsRef<str>>(&self, cache: &mut Cache, pkgs: &[S]) -> Result<(), Error>;
-    /// Perform an info request with package splitting to avoid the URI length limit.
-    fn info_ext<S: AsRef<str>>(&self, pkgs: &[S]) -> Result<Vec<Package>, Error>;
-}
-
-impl RaurExt for Handle {
     //TODO: async
-    fn cache_info<S: AsRef<str>>(&self, cache: &mut Cache, pkgs: &[S]) -> Result<(), Error> {
+    fn cache_info<S: AsRef<str>>(
+        &self,
+        cache: &mut Cache,
+        pkgs: &[S],
+    ) -> Result<Vec<crate::Package>, Self::Err> {
+        let mut ret = Vec::with_capacity(pkgs.len());
         let pkgs = pkgs
             .iter()
             .filter(|p| !cache.contains(p.as_ref()))
@@ -21,14 +20,21 @@ impl RaurExt for Handle {
 
         for chunk in pkgs.chunks(100) {
             let res = self.info(chunk)?;
-            cache.extend(res.into_iter().map(Into::into));
+            cache.reserve(chunk.len());
+            ret.reserve(chunk.len());
+            for pkg in res.into_iter() {
+                let pkg = crate::Package::from(pkg);
+                cache.insert(pkg.clone());
+                ret.push(pkg);
+            }
         }
 
-        Ok(())
+        Ok(ret)
     }
 
+    /// Perform an info request with package splitting to avoid the URI length limit.
     //TODO: async
-    fn info_ext<S: AsRef<str>>(&self, pkgs: &[S]) -> Result<Vec<Package>, Error> {
+    fn info_ext<S: AsRef<str>>(&self, pkgs: &[S]) -> Result<Vec<Package>, Self::Err> {
         let mut packages = Vec::with_capacity(pkgs.len());
 
         for chunk in pkgs.chunks(100) {
@@ -39,3 +45,5 @@ impl RaurExt for Handle {
         Ok(packages)
     }
 }
+
+impl<T> RaurExt for T where T: Raur {}
